4f23a4a77b4338d0cfedf55c95a465e8deca4de0
vijay <vijay.25b01010780@abes.ac.in>
2026-09-02 14:49:43 +0530

    constructors

---
+++ SEP/2/chaining.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class player{
+    private:
+        string name;
+        int age;
+    public:
+        player(string n, int a){
+            name = n;
+            age = a;
+            cout << "Player " << name << " created." << endl;
+        }
+
+        //chained constructor
+        player(string n): player(n, 0){
+             cout << "Player " << name << " created with default age." << endl;
+}};
+
+int main(){
+    player p1("John", 25);
+    player p2("Alice");
+    return 0;
+}
